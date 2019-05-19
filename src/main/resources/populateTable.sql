e18a077275911e176c4388c0fde832f23201d83d
lauragiza <lauragiza17@gmail.com>
2019-05-19 17:00:45 +0200

    sql script for populate basic data

---
+++ src/main/resources/populateTable.sql
@@ -0,0 +1,20 @@
+INSERT INTO PATIENT (name, surname) VALUES ('Olga','Nowak');
+INSERT INTO PATIENT (name, surname) VALUES ('Maciej','Mazur');
+INSERT INTO PATIENT (name, surname) VALUES ('Katarzyna','Tomczyk');
+INSERT INTO PATIENT (name, surname) VALUES ('Anna','Kot');
+INSERT INTO PATIENT (name, surname) VALUES ('Marcin','Ambroziak');
+INSERT INTO PATIENT (name, surname) VALUES ('Łukasz','Tinek');
+
+INSERT INTO DOCTOR (name, lastname, specialization, phone, start_working_time,end_working_time) VALUES ('Tadeusz','Linek','Pediatra','523456789','8:00','15:00');
+INSERT INTO DOCTOR (name, lastname, specialization, phone, start_working_time,end_working_time) VALUES ('Anna','Karmen','Chirurg','523456781','8:00','16:00');
+INSERT INTO DOCTOR (name, lastname, specialization, phone, start_working_time,end_working_time) VALUES ('Ireneusz','Man','Ginekolog','523456789','9:00','15:00');
+INSERT INTO DOCTOR (name, lastname, specialization, phone, start_working_time,end_working_time) VALUES ('Magda','Walczak','Ortopeda','523456489','8:00','15:00');
+INSERT INTO DOCTOR (name, lastname, specialization, phone, start_working_time,end_working_time) VALUES ('Zofia','Mendoza','Dermatolog','523456789','10:00','15:00');
+INSERT INTO DOCTOR (name, lastname, specialization, phone, start_working_time,end_working_time) VALUES ('Wiktor','Krakowiak','Dentysta','523456789','8:00','14:00');
+
+INSERT INTO OFFICE (number, name) VALUES (100,'Gabinet Przyjęć');
+INSERT INTO OFFICE (number, name) VALUES (101,'Gabinet Przyjęć');
+INSERT INTO OFFICE (number, name) VALUES (102,'Gabinet Przyjęć');
+INSERT INTO OFFICE (number, name) VALUES (103,'Gabinet Przyjęć');
+INSERT INTO OFFICE (number, name) VALUES (104,'Gabinet Przyjęć');
+INSERT INTO OFFICE (number, name) VALUES (105,'Gabinet Przyjęć');
